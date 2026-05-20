6188e4c69c36648efca38f6543f03590c58b9aea
Seif Mohamed <20p1710@eng.asu.edu.eg>
2026-05-20 22:35:41 +0300

    feat(helpers): add filterByField method

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,3 +4,11 @@ export function findById<T extends { id: string }>(
 ): T | undefined {
   return items.find((item) => item.id === id);
 }
+
+export function filterByField<T, K extends keyof T>(
+  items: T[],
+  field: K,
+  value: T[K],
+): T[] {
+  return items.filter((item) => item[field] === value);
+}
