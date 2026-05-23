fe8170d3cfdbf3fb795abc11214380cdbc165ed9
Seif Mohamed <20p1710@eng.asu.edu.eg>
2026-05-23 16:31:12 +0300

    feat(currency): add fetchExchangeRates method

---
+++ src/services/currency.ts
@@ -0,0 +1,15 @@
+import { ExchangeRate } from "../types";
+import { isExchangeRate } from "../utils/validators";
+
+export async function fetchExchangeRates(): Promise<ExchangeRate> {
+  const response = await fetch("https://open.er-api.com/v6/latest/USD");
+  if (!response.ok) {
+    throw new Error("Response of exchange data failed");
+  }
+  const data: unknown = await response.json();
+
+  if (!isExchangeRate(data)) {
+    throw new Error("Not a correct type of what we have");
+  }
+  return data;
+}
